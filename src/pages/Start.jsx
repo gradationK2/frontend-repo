9d982038b4747dcabc7ff7f0003fd4d3b53f821a
mzxxzysy <jsyjj21@gmail.com>
2025-02-16 00:22:29 +0900

    1차 커밋

--- src/pages/Start.jsx
+++ src/pages/Start.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 import * as A from "../styles/StartStyle";
+import logo from "../assets/logo.svg";
 
 function Start() {
-  return <A.Start></A.Start>;
+  return (
+    <A.Start>
+      <A.Img src={logo} />
+    </A.Start>
+  );
 }
 
 export default Start;
--- src/styles/StartStyle.js
+++ src/styles/StartStyle.js
@@ -4,12 +4,7 @@ export const Start = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
+  padding: 55px;
 `;
-export const Img = styled.div`
-  width: 281px;
-  height: 286px;
 
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
+export const Img = styled.img``;
