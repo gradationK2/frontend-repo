9819bc43436f05106e4d25dcdb834d7f006ed0d3
sujin <csujin2040@gmail.com>
2025-02-16 00:11:09 +0900

    🎉 핸드폰화면 설정

---
+++ src/styles/CommonStyle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+import color from "./color";
+
+export const Common = styled.div`
+  width: 393px;
+  max-width: 100%;
+  min-height: 100vh; 
+  margin: 0 auto; 
+  background: ${color.white};
+  color: ${color.black};
+  display: flex;
+  flex-direction: column;
+`;
